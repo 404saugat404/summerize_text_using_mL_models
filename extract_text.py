1f4167bd22c10cb5f3edcc0e02da902c12ce0aae
saugat <saugat556513@gmail.com>
2024-03-19 10:13:52 +0545

    saved extracted text to json format

--- extract_text.py
+++ extract_text.py
@@ -1,15 +1,19 @@
-import PyPDF2
+import pdfplumber
+import json
 
 def extract_text_from_pdf(pdf_path):
     text = ""
-    with open(pdf_path, "rb") as file:
-        pdf_reader = PyPDF2.PdfReader(file)
-        num_pages = len(pdf_reader.pages)
-        for page_num in range(num_pages):
-            page = pdf_reader.pages[page_num]
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
             text += page.extract_text()
     return text
 
+def save_text_to_json(text, json_path):
+    with open(json_path, "w") as json_file:
+        json.dump({"text": text}, json_file, indent=4)
+
 # Replace "your_pdf_file.pdf" with the path to your PDF file
-pdf_text = extract_text_from_pdf("/home/saugat/Desktop/summerize_text/hello.pdf")
-print(pdf_text)
+pdf_text = extract_text_from_pdf("/home/saugat/Desktop/summerizer_project/hello.pdf")
+
+# Replace "output.json" with the desired path for your JSON file
+save_text_to_json(pdf_text, "output.json")
